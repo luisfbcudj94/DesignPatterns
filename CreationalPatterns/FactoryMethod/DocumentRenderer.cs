b3204fde7b0dc91f6779543a9f010c8f415f7ac6
LuisPerez <alejandroabril98@gmail.com>
2024-09-11 05:48:39 -0500

    feat: example of factory method

---
+++ CreationalPatterns/FactoryMethod/DocumentRenderer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class DocumentRenderer
+    {
+        public void RenderDocument(DocumentFactory factory)
+        {
+            IDocument document = factory.CreateDocument();
+            Console.WriteLine(document.Render());
+        }
+    }
+}
---
+++ CreationalPatterns/FactoryMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FactoryMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DocumentRenderer renderer = new DocumentRenderer();
+
+            DocumentFactory wordFactory = new WordDocumentFactory();
+            DocumentFactory pdfFactory = new PDFDocumentFactory();
+
+            renderer.RenderDocument(wordFactory); 
+            renderer.RenderDocument(pdfFactory);  
+        }
+    }
+}
